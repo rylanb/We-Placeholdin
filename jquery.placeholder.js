905c362e189ef22746390fcb69ea5c75b06b3060
Rylan Bowers <rylan@quickleft.com>
2011-05-02 16:06:58 -0600

    Fixed to prevent early return out of this.each when non placeholder attr input is included.

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -16,50 +16,49 @@
     });
 
     return this.each(function() {
-      /* Check for Placeholder support natively in the browser here if you aren't including Modernizr - modernizr.com/*/
-
+      /* Check for Placeholder support natively in the browser here if you aren't including Modernizr - modernizr.com*/
       var $this = $(this),
           val = $this.attr('placeholder');
 
       /*What is this!? Get outta here with that non input/text area nonsense!*/
       if($this[0].nodeName != "INPUT" && $this[0].nodeName != "TEXTAREA"){return false;}
 
-      /* checks if placeholder attribute exists
-      Return if doesn't exist so you don't empty fields with values in them */
-      if(val === "" || val === undefined){return false;}
+      /* checks if placeholder attribute exists*/
 
       /*Let's start the fun! Check what type of field you are dealing with.
       Textareas and inputs of type email/text are checked for now.
       Email is passed as type text, just included in case a browser gets uppity later */
-      if($this.attr('type') === "text" || $this.attr('type') === "email" || $this[0].nodeName === "TEXTAREA"){
-        $this.attr('title', val);
-        $this.val(val).focus(function(){
-          if($this.attr('title') === $this.val()){
-            $this.val("");
-          }
-        }).blur(function(){
-          if( $this.val() === "" ){
-            $this.val(val);
-          }
-        });
-      }
-      else if($this.attr('type') === "password"){
-        var $offset = $this.offset();
-        $('body').append("<span class='pass-span pldr-" + $this.attr('id') + "'>" + val + "</span>");
-        $('.pldr-' + $this.attr('id')).css("position", "absolute").css("top", $offset.top + 8).css("left", $offset.left + 5).css("z-index", "9999");
-        $this.focus(function(){
-            $('.pldr-' + $this.attr('id')).hide();
-        })
-        .blur(function(){
-          if( $this.val() === "" ){
-            $('.pldr-' + $this.attr('id')).show();
-          }
-        });
-        $('.pass-span').click(function(){
-          $(this).hide();
-          var $focusInputTo = "#" + $(this).attr('class').split(' ')[1].replace("pldr-", "");
-          $($focusInputTo).focus();
-        });
+      if( $this.val().length === 0 && val !== "" && val !== undefined ) {
+        if($this.attr('type') === "text" || $this.attr('type') === "email" || $this[0].nodeName === "TEXTAREA"){
+          $this.attr('title', val);
+          $this.val(val).focus(function(){
+            if($this.attr('title') === $this.val()){
+              $this.val("");
+            }
+          }).blur(function(){
+            if( $this.val() === "" ){
+              $this.val(val);
+            }
+          });
+        }
+        else if($this.attr('type') === "password"){
+          var $offset = $this.offset();
+          $('body').append("<span class='pass-span pldr-" + $this.attr('id') + "'>" + val + "</span>");
+          $('.pldr-' + $this.attr('id')).css("position", "absolute").css("top", $offset.top + 8).css("left", $offset.left + 5).css("z-index", "9999");
+          $this.focus(function(){
+              $('.pldr-' + $this.attr('id')).hide();
+          })
+          .blur(function(){
+            if( $this.val() === "" ){
+              $('.pldr-' + $this.attr('id')).show();
+            }
+          });
+          $('.pass-span').click(function(){
+            $(this).hide();
+            var $focusInputTo = "#" + $(this).attr('class').split(' ')[1].replace("pldr-", "");
+            $($focusInputTo).focus();
+          });
+        }
       }
     });
 
